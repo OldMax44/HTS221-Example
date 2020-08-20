2abfc48f61b40ad3984dc19a3a9c3f387e57d479
Max44 <max_day_az@yahoo.com>
2020-08-20 14:55:49 -0400

    Add files via upload

---
+++ Nordic/hts221_oneshot/pca10056/blank/ses/Output/hts221_sensor_pca10056 Debug/Obj/app_error.d
@@ -0,0 +1,23 @@
+Output/hts221_sensor_pca10056\ Debug/Obj/app_error.o: \
+ E:\Nordic\SDK\ 17.0.0\nRF5_SDK_17.0.0_9d13099\components\libraries\util\app_error.c \
+ ../../../../../../modules/nrfx/mdk/nrf.h \
+ ../../../../../../modules/nrfx/mdk/nrf52840.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52c/include/stdint.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_version.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/mpu_armv7.h \
+ ../../../../../../modules/nrfx/mdk/system_nrf52840.h \
+ ../../../../../../modules/nrfx/mdk/nrf52840_bitfields.h \
+ ../../../../../../modules/nrfx/mdk/nrf51_to_nrf52840.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_to_nrf52840.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52c/include/stdio.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52c/include/__crossworks.h \
+ E:\Nordic\SDK\ 17.0.0\nRF5_SDK_17.0.0_9d13099\components\libraries\util\app_error.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52c/include/stdbool.h \
+ E:\Nordic\SDK\ 17.0.0\nRF5_SDK_17.0.0_9d13099\components\libraries\util\sdk_errors.h \
+ ../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_error.h \
+ E:\Nordic\SDK\ 17.0.0\nRF5_SDK_17.0.0_9d13099\components\libraries\util\nordic_common.h \
+ E:\Nordic\SDK\ 17.0.0\nRF5_SDK_17.0.0_9d13099\components\libraries\util\app_error_weak.h
---
+++ Nordic/hts221_oneshot/pca10056/blank/ses/Output/hts221_sensor_pca10056 Debug/Obj/ses_startup_nrf_common.d
@@ -0,0 +1,2 @@
+Output/hts221_sensor_pca10056\ Debug/Obj/ses_startup_nrf_common.o: \
+ E:\Nordic\SDK\ 17.0.0\nRF5_SDK_17.0.0_9d13099\modules\nrfx\mdk\ses_startup_nrf_common.s
